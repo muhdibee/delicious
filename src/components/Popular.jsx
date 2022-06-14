38bee50cb1b4a7a9ffee62aec5e2c7f73e8b03cc
Muhammad <muhdibee@gmail.com>
2022-06-14 17:50:59 +0100

    Getting recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function Popular() {
+
+  const [popular, setPopular,] = useState([]);
+
+  useEffect(()=>{
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api=await  fetch(`https://api.spoonacular.com/recipes/random?apiKey=e43fa5857cbe4e37af330001efec3000&number=9`);
+    const data=await api.json();
+    console.log(data);
+    setPopular(data.recipes);
+
   }
   return (
-    <div>Popular</div>
+    <div>Popular Receipes: {console.log(popular)}
+        {popular.map((recipe)=> {
+          return(
+            <div>
+              <p>{recipe.title}</p>
+            </div>
+          )
+        })
+        }
+    </div>
   )
 }
 
